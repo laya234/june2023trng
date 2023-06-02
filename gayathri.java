03450d57c9c46aa1f180aee408e25bba5d6fa28a
laya234 <131333363+laya234@users.noreply.github.com>
2023-06-02 04:41:39 -0700

    Add files via upload

---
+++ gayathri.java
@@ -0,0 +1,16 @@
+class Gayi{
+public void cricket(){
+System.out.println("gayi lost it");
+}
+public void chess(){
+System.out.println("gayi won it");
+}
+}
+class Laya
+{
+public static void main(String args[]){
+Gayi ob=new Gayi();
+ob.cricket();
+ob.chess();
+}
+}
